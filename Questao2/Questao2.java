9079b405c6cbcb524470931cb2889091d037e007
Marco A S <aureliojuniorcmrj@hotmail.com>
2022-05-25 10:21:04 -0300

    solved bug of comparing substrings; added reading loop in main

--- Questao2/Questao2.java
+++ Questao2/Questao2.java
@@ -5,11 +5,11 @@ public class Questao2 {
 
 
   public static boolean ConcatERemove(String s, String t, int k) {
-    boolean resp = false;
-    int count = 0;
+    boolean resp = false; /* variable to be returned at the end of this method */
+    int count = 0; /* counter to be added until it matches k */
 
-    // removing last char of s until both strings become equal
-    while(!t.contains(s) && count < k) {  
+    // removing last char of s until t starts with s
+    while(!t.startsWith(s) && count < k) {  
       count++;
       s = s.substring(0, s.length() - 1);
     }
@@ -21,7 +21,7 @@ public class Questao2 {
     }
     
     if(s.equals(t)) {
-      resp = true;
+      resp = true; /* updates the response to be returned */
     }
 
     return resp;
@@ -29,10 +29,18 @@ public class Questao2 {
 
 
   public static void main(String[] args) {
-    String s = scanner.next();
-    String t = scanner.next();
-    int k = scanner.nextInt();
+    boolean repeat = true; /* variable to maintain the reading loop */
+    do {
+      String s = scanner.next(); /* read the initial string from standard input */
+      if(s.equals("FIM")) { /* if test is true, the program ends */
+        repeat = false;
+      } 
+      else {
+        String t = scanner.next(); /* read the desired string from standard input */
+        int k = scanner.nextInt(); /* read the number of operations from standard input */
+        System.out.println(ConcatERemove(s,t,k)? "Sim" : "Não"); /* print the result */
+      }
+    } while (repeat);
 
-    System.out.println(ConcatERemove(s,t,k)? "Sim" : "Não");
   }
 }
