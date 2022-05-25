f5fde8169e7f9b7d737c7bd48e88be17a683026c
Marco A S <aureliojuniorcmrj@hotmail.com>
2022-05-25 11:14:59 -0300

    changed variable names

--- Questao1/Questao1.java
+++ Questao1/Questao1.java
@@ -1,18 +1,15 @@
 public class Questao1 {
   public static void main(String[] args) {
-    int num = 1, max = 100;
-    for (int i = num ; i<=max; i++) {
-      boolean divPor3 = (i%3 == 0), divPor5 = (i%5 == 0);
-      if (divPor3 && divPor5) {
+    final int start = 1, end = 100; /* limits set on the stated question */
+    for (int i = start ; i<=end; i++) {
+      boolean divPor3 = (i%3 == 0), divPor5 = (i%5 == 0); /* variables for ease of reading */
+      if (divPor3 && divPor5) { /* if divisible by 3 and 5, print FooBaa */
         System.out.println("FooBaa");
-      }
-      else if (divPor3) {
+      } else if (divPor3) { /* if divisible by 3, print Foo */
         System.out.println("Foo");
-      } 
-      else if (divPor5) {
+      } else if (divPor5) { /* if divisible by 5, print Baa */
         System.out.println("Baa");
-      }
-      else {
+      } else {
         System.out.println(i);
       }
     }
